bb0b068725b7a5ef4e4b9e43a4be06c2ede5fcd3
LuisAlejandro Contreras González <lcontreras14@ucol.mx>
2019-08-23 20:23:35 -0500

    Ciclo Terminado

---
+++ Ciclos FOR/Ciclos FOR/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ciclos_FOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Crear un ciclo donde el usuario escoga donde inicia, termina y de cuanto en cuanto ira");
+            Console.WriteLine("De donde empezara el ciclo? ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Donde acabara el ciclo? ");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto ira el ciclo? ");
+            z = int.Parse(Console.ReadLine());
+            if(x>y)
+            {
+                for (int i=x;i>y;i-=z)
+                {
+                    Console.WriteLine("i= " + i);
+                }
+            }
+            else
+            {
+                for(int i=x;i<y;i+=z)
+                {
+                    Console.WriteLine("i= " + i);
+                }
+            }
+        }
+    }
+}
